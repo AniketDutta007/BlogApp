ec58e93308e64bc593ee6fad48ef34abd6f803a3
AniketDutta007 <aniket2018dutta@gmail.com>
2022-03-17 00:09:59 +0530

    added create post and home feature

---
+++ src/Hooks.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+export function useFormInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+  function handleChange(event) {
+    setValue(event.target.value);
+  }
+  return {
+    value: value,
+    onChange: handleChange,
+  };
+}
--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,5 +1,38 @@
+import { useFormInput } from '../Hooks';
+import { firestore } from '../firebase';
+
 function CreatePost() {
-  return <div className="">CreatePost</div>;
+  const title = useFormInput('');
+  const subTitle = useFormInput('');
+  const content = useFormInput('');
+  function handleSubmit(event) {
+    firestore.collection('posts').add({
+      title: title.value,
+      subTitle: subTitle.value,
+      content: content.value,
+      createdAt: new Date(),
+    });
+  }
+  return (
+    <div className="create-post">
+      <h1>Create Post</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-field">
+          <label>Title</label>
+          <input {...title} />
+        </div>
+        <div className="form-field">
+          <label>Sub Title</label>
+          <input {...subTitle} />
+        </div>
+        <div className="form-field">
+          <label>Content</label>
+          <textarea {...content}></textarea>
+        </div>
+        <button className="create-post-btn">Create Post</button>
+      </form>
+    </div>
+  );
 }
 
 export default CreatePost;
