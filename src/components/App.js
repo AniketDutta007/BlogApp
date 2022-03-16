37fc2de019e753d6dfa180fa6f5f81d5340ef011
AniketDutta007 <aniket2018dutta@gmail.com>
2022-03-16 17:33:15 +0530

    create components and add styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import { Navbar, Home, PostDetail, CreatePost } from './';
+
 function App() {
-  return <div className="App">React Blog App</div>;
+  return (
+    <div className="container">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/post/:postId" element={<PostDetail />} />
+        <Route path="/create-post" element={<CreatePost />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Navbar from './Navbar';
+import Home from './Home';
+import PostDetail from './PostDetail';
+import CreatePost from './CreatePost';
+
+export { Navbar, Home, PostDetail, CreatePost };
